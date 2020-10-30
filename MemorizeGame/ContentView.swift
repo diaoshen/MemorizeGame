cb525f49a10d4bd4959010147d40cf0c32609e9b
DIAO <isolvethis@gmail.com>
2020-10-29 19:54:23 -0700

    Setup of CardView with 4 Cards placed horizontally. End of Lecture 1

--- MemorizeGame/ContentView.swift
+++ MemorizeGame/ContentView.swift
@@ -5,12 +5,36 @@
 //  Created by DIAO on 10/28/20Wednesday.
 //
 
+/*
+    This version creates 4 Cards and arranges them horizontally. Using ForEach with ZStack then HStack it
+    Displaying Card's Front or Back is based on the value set in CardView.isFaceUp
+ 
+    End of Lecture 1 
+ */
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) { index in
+                CardView()
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        let isFaceUp: Bool = false
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                
+            }
+        }
     }
 }
 
